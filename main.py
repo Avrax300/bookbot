17c118bcbb0078fa1241e247a143c0a2e18ae589
Avrax300 <pruneausebastien@gmail.com>
2025-08-13 22:28:38 -0400

    allowing to specify books in argument

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from stats import get_word_count
 from stats import get_character_count
 from stats import sort_characters
+import sys
 
 # converts the content of frankenstein.txt as a string.
 def get_book_text():
-    with open("[PATH_TO/BOOK]") as f:
+    with open(f"{sys.argv[1]}") as f:
         text = f.read()
         return text
 
 def main():
-    text = get_book_text()
-    print(f"============ BOOKBOT ============")
-    print(f"Analyzing book found at [PATH_TO/BOOK]...")
-    print(f"----------- Word Count ----------")
-    get_word_count(text)
-    character_count = get_character_count(text)
-    print(f"--------- Character Count -------")
-    sort_characters(character_count)
-    print(f"============= END ===============")
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        text = get_book_text()
+        print(f"============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print(f"----------- Word Count ----------")
+        get_word_count(text)
+        character_count = get_character_count(text)
+        print(f"--------- Character Count -------")
+        sort_characters(character_count)
+        print(f"============= END ===============")
 
 main()
